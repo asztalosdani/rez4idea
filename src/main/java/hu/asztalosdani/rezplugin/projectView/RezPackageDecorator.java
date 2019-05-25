0c14fa97064350387c72a6a5e1eea0e4ba38fa80
asztalosdani <asztalosdani@gmail.com>
2019-05-25 13:10:18 +0200

    Icons for action and toolWindow

--- src/main/java/hu/asztalosdani/rezplugin/projectView/RezPackageDecorator.java
+++ src/main/java/hu/asztalosdani/rezplugin/projectView/RezPackageDecorator.java
@@ -4,19 +4,15 @@ import com.intellij.icons.AllIcons;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.ProjectViewNode;
 import com.intellij.ide.projectView.ProjectViewNodeDecorator;
-import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.packageDependencies.ui.PackageDependenciesNode;
 import com.intellij.ui.ColoredTreeCellRenderer;
 import com.intellij.ui.RowIcon;
+import util.RezIcons;
 
 import javax.swing.*;
 
 public class RezPackageDecorator implements ProjectViewNodeDecorator {
-
-    private Icon packageFileIcon = IconLoader.getIcon("/icons/packageFile.png");
-    private Icon packageFolderIcon = IconLoader.getIcon("/icons/packageFolder.png");
-    private Icon packageSourcesRootIcon = IconLoader.getIcon("/icons/packageSourcesRoot.png");
 
     @Override
     public void decorate(ProjectViewNode node, PresentationData data) {
@@ -31,16 +27,16 @@ public class RezPackageDecorator implements ProjectViewNodeDecorator {
                 Icon[] allIcons = ((RowIcon) originalIcon).getAllIcons();
                 for (Icon icon : allIcons) {
                     if (icon == AllIcons.Modules.SourceRoot) {
-                        data.setIcon(packageSourcesRootIcon);
+                        data.setIcon(RezIcons.packageSourcesRootIcon);
                     }
                 }
             } else if (originalIcon == AllIcons.Modules.SourceRoot) {
-                data.setIcon(packageSourcesRootIcon);
+                data.setIcon(RezIcons.packageSourcesRootIcon);
             } else if (originalIcon == AllIcons.Nodes.Folder) {
-                data.setIcon(packageFolderIcon);
+                data.setIcon(RezIcons.packageFolderIcon);
             }
         } else if (file.getName().equals("package.py")) {
-            data.setIcon(packageFileIcon);
+            data.setIcon(RezIcons.packageFileIcon);
         }
     }
 
---
+++ src/main/java/util/RezIcons.java
@@ -0,0 +1,11 @@
+package util;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface RezIcons {
+    Icon packageFileIcon = IconLoader.getIcon("/icons/packageFile.png");
+    Icon packageFolderIcon = IconLoader.getIcon("/icons/packageFolder.png");
+    Icon packageSourcesRootIcon = IconLoader.getIcon("/icons/packageSourcesRoot.png");
+}
